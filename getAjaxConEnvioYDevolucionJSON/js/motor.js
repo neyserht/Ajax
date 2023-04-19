0e7145a9759ac1ed3cf1cf517f0061dd133cfa98
neyserht <neyser.nht@gmail.com>
2023-04-19 10:16:58 +0200

    terminado

--- getAjaxConEnvioYDevolucionJSON/js/motor.js
+++ getAjaxConEnvioYDevolucionJSON/js/motor.js
@@ -1,42 +1,59 @@
-function peticionServidor(d1, d2, servidor1)
+function peticionServidor(c1, c2, d1, d2, servidor1)
 {
     //alert("hola con el evento en JS");
     let ajax1 = new XMLHttpRequest();
+    
+    let objJSON1 = {
+        "nom" : c1.value,
+        "tlf" : c2.value
+    };
+
+    objJSON1 = JSON.stringify(objJSON1);
     
     ajax1.onreadystatechange = function()
     {
         if (ajax1.readyState==4 && ajax1.status==200)
         {
-            let objJSON1 =  JSON.parse(ajax1.responseText);
-            document.getElementById(d1.id).innerHTML = "Nombre: " + objJSON1.nombre;
-            document.getElementById(d2.id).innerHTML = "Telefono: " + objJSON1.tlf;
+            objJSON1 = JSON.parse(ajax1.responseText);
+            document.getElementById(d1.id).innerHTML = objJSON1.nom;
+            document.getElementById(d2.id).innerHTML = objJSON1.tlf;
         }
     };
 
-    ajax1.open('GET', servidor1, true);
+    ajax1.open('GET', servidor1 + "?datosJSON1="+ objJSON1, true);
     ajax1.send();
 }
-
 
 // Cuando todos los recursos se hayan cargado se añade el escuchador de eventos
 window.addEventListener("load", function(event)
 {
 
     //Establecer una referencia de los elementos
+    // Button
     const b1 = document.getElementById("b1");
+    
+    // Input
+    const c1 = document.getElementById("c1");
+    const c2 = document.getElementById("c2");
+
+    // Div
+    const d1 = document.getElementById("d1");
+    const d2 = document.getElementById("d2");
+    
+    // Servidor
     const servidor1 = "servidor.php";
+
     
     // Asociar el elemento al evento y llamada a la funcion
     if(b1)
     {
 
-        const d1 = document.getElementById("d1");
-        const d2 = document.getElementById("d2");
+        
 
         b1.addEventListener("click", function(event)
         {
             event.preventDefault();
-            peticionServidor(d1, d2, servidor1);
+            peticionServidor(c1, c2, d1, d2, servidor1);
 
         });
     }
--- getAjaxConEnvioYDevolucionJSON/servidor.php
+++ getAjaxConEnvioYDevolucionJSON/servidor.php
@@ -1,13 +1,20 @@
 <?php
     
-    // Declaración de variables
-    $nombre = "Neyser";
-    $tlf = "123";
+    // Recepcion de los datos con JSON
+    header('Content-Type: application/json; charset=utf-8');
+
+    // Cuando es true, los bjetos JSON devueltos seran converids a arrays asociativos
+    // Cuando es false, los bjetos JSON devueltos seran converids a objetos
+    
+    $datosJSON1 = json_decode($_GET['datosJSON1'], false);
+    $nom = "Nombre: " . $datosJSON1->nom;
+    $tlf = "Telefono: " . $datosJSON1->tlf;
+
 
     // Devolución de datos como JSON
-    $data = array('nombre' => $nombre, 'tlf' => $tlf);
+    $datos = array('nom' => $nom, 'tlf' => $tlf);
     
-    echo json_encode($data);
+    echo json_encode($datos);
     //echo $data;
 
 
