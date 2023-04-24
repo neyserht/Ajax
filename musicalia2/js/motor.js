a54ec653bddf112c09a6524b688bc923bc7aae2f
neyserht <neyser.nht@gmail.com>
2023-04-24 14:05:01 +0200

    Musicalia 2 terminado

--- musicalia2/js/motor.js
+++ musicalia2/js/motor.js
@@ -1,4 +1,4 @@
-function peticionServidor(p01, audio, servidor1)
+function peticionServidor(disco, servidor1)
 {
     //alert("hola con el evento en JS");
     let ajax1 = new XMLHttpRequest();
@@ -11,53 +11,37 @@ function peticionServidor(p01, audio, servidor1)
         }
     };
 
-    ajax1.open('GET', servidor1 + '?pista='+ p01.name , true);
+    ajax1.open('GET', servidor1 + '?pista='+ disco.id , true);
     ajax1.send();
 
 }
 
-function peticionGeneral(pista, servidor1){
-    peticionServidor(p01, audio, servidor1);
-
-}
-
-
 // Cuando todos los recursos se hayan cargado se añade el escuchador de eventos
+
 window.addEventListener("load", function(event)
 {
 
     //Establecer una referencia de los elementos
-    const pista = this.document.getElementsByName("pista");
+    const disco = this.document.getElementsByName("disco");
 
-    const p01 = this.document.getElementById("p01");
-    const p02 = this.document.getElementById("p02");
-    const p03 = this.document.getElementById("p03");
-    const p04 = this.document.getElementById("p04");
-
-    const audio = document.getElementById("audio");
 
     const servidor1 = "servidor.php";  
     
     // Asociar el elemento al evento y llamada a la funcion
- 
-    if(p01){
+    
+    if(disco){
 
-        for(var i = 0; i < pista.length; i++) {
-            (function(index) {
-                pista[index].addEventListener("click", function(event) {
-                    event.preventDefault();
-                    peticionServidor(p01, audio, servidor1);
-                 console.log("Clicked index: " + index);
-               })
-            })(i);
-          }
+        for (let index = 0; index < disco.length; index++) {
+            let pista = disco[index];
+            //element.addEventListener("click", function(event)
+            //console.log(pista);
+            pista.addEventListener("click", function(event)
+            {
+                event.preventDefault();
+                peticionServidor(pista, servidor1);
+            })
+        };
 
-        // pista.addEventListener("click", function(event)
-        // {
-        //     event.preventDefault();
-        //     peticionServidor(p01, audio, servidor1);
-
-        // });
     }
 
 });
